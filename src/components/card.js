e59a52ea3ee23c9ca6cfe609a5a81c285157b51a
giusbruno <giuseppebruno30081999@gmail.com>
2018-08-27 08:54:48 +0200

    initial commit

--- src/components/card.js
+++ src/components/card.js
@@ -5,17 +5,29 @@ import fire from './fire';
 class Card extends React.Component{
     constructor(props) {
 		super(props);
-		this.handleClick = this.handleClick.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+        this.state={act:true};
+    
 	}
 
- handleClick(){
-    console.log('ciao')
-    fire.database().ref(this.props.st+'/'+this.props.name+'/val').set('1');    
+ async handleClick(){
+  
+    let aef = fire.database().ref(this.props.st+'/'+this.props.name+'/val');
+    aef.set("1");
+
+    this.setState({act:false});  
+    aef.on('value', function(snapshot) {
+        
+       if(snapshot.val()=='0'){
+       this.setState({act:true})};
+    }.bind(this));
  }
+
  render(){
+    let c_class = this.state.act ? "cont" : "working";
 	 return (
          
-    <div className="cont" onClick={this.handleClick}>
+    <div onClick={this.handleClick} className={c_class}>
     <div className="icon" style={{backgroundImage:"url('"+this.props.ind+"')"}} >
     </div>
     <div className="ogg">{this.props.name}</div>
